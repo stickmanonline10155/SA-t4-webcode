a90e4e113d2a947e69a4d258e7cc08b7cff720a6
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2023-05-01 21:59:43 +0800

    Update note-insert.php

--- note-insert.php
+++ note-insert.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/937e93c93c.js" crossorigin="anonymous"></script>
     <title>筆記編輯</title>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.tiny.cloud/1/12f7tfrytkoj49tdwch4ft81l3qgcp7cd36l7e4gr45irzy7/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
 </head>
 <body style="background-color: rgb(230, 230, 230);">
@@ -29,84 +29,41 @@
     </div>
     <div>
         <div class="note-tool">
-            <div class="check-button" align="right">
-                <input type=submit value = "儲存">
-            </div>
+            
         </div>
     </div>
     <!--筆記編輯-->
+    <form>
+      <textarea id="basic-example">
         
-<textarea id="basic-example">
-  <p><img style="display: block; margin-left: auto; margin-right: auto;" title="Tiny Logo" src="https://www.tiny.cloud/docs/images/logos/android-chrome-256x256.png" alt="TinyMCE Logo" width="128" height="128" /></p>
-  <h2 style="text-align: center;">Welcome to the TinyMCE editor demo!</h2>
+      </textarea>
+      <button name="submitbtn"></button>
+      <script>
+      tinymce.init({
+        selector: 'textarea#basic-example',
+        height: 500,
+        menubar: false,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount','searchreplace',
+          'save'
+          
+        ],
+        toolbar: 'undo redo | formatselect | ' +
+        'bold italic backcolor | alignleft aligncenter ' +
+        'alignright alignjustify | bullist numlist outdent indent | ' +
+        'removeformat | help |'+'searchreplace | save',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        
+      });
+      tinymce.activeEditor.execCommand('mceSave');
+      tinymce.activeEditor.execCommand('SearchReplace');
 
-  <h2>Got questions or need help?</h2>
+      
+      </script>
+    </form>    
 
-  <ul>
-    <li>Our <a href="https://www.tiny.cloud/docs/">documentation</a> is a great resource for learning how to configure TinyMCE.</li>
-    <li>Have a specific question? Try the <a href="https://stackoverflow.com/questions/tagged/tinymce" target="_blank" rel="noopener"><code>tinymce</code> tag at Stack Overflow</a>.</li>
-    <li>We also offer enterprise grade support as part of <a href="https://www.tiny.cloud/pricing">TinyMCE premium plans</a>.</li>
-  </ul>
-
-  <h2>A simple table to play with</h2>
-
-  <table style="border-collapse: collapse; width: 100%;" border="1">
-    <thead>
-      <tr>
-        <th>Product</th>
-        <th>Cost</th>
-        <th>Really?</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>TinyMCE</td>
-        <td>Free</td>
-        <td>YES!</td>
-      </tr>
-      <tr>
-        <td>Plupload</td>
-        <td>Free</td>
-        <td>YES!</td>
-      </tr>
-    </tbody>
-  </table>
-
-  <h2>Found a bug?</h2>
-
-  <p>
-    If you think you have found a bug please create an issue on the <a href="https://github.com/tinymce/tinymce/issues">GitHub repo</a> to report it to the developers.
-  </p>
-
-  <h2>Finally ...</h2>
-
-  <p>
-    Don't forget to check out our other product <a href="http://www.plupload.com" target="_blank">Plupload</a>, your ultimate upload solution featuring HTML5 upload support.
-  </p>
-  <p>
-    Thanks for supporting TinyMCE! We hope it helps you and your users create great content.<br>All the best from the TinyMCE team.
-  </p>
-</textarea>
-
-<script>
-tinymce.init({
-  selector: 'textarea#basic-example',
-  height: 500,
-  menubar: false,
-  plugins: [
-    'advlist autolink lists link image charmap print preview anchor',
-    'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table paste code help wordcount'
-  ],
-  toolbar: 'undo redo | formatselect | ' +
-  'bold italic backcolor | alignleft aligncenter ' +
-  'alignright alignjustify | bullist numlist outdent indent | ' +
-  'removeformat | help',
-  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-});
-
-</script>
-    </form>
-
+    
 </body>
 </html>
